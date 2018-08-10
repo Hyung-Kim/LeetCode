25b1f5e3653b2c8d3df6d4649ed475a23d831d78
kim.taehyung <xodaktmdnj@naver.com>
2018-08-11 01:12:05 +0900

    8월 2주차 알고리즘 스터디

---
+++ MergeTwoBinaryTrees.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 != default(TreeNode))
+            {
+                t1.val += t2?.val ?? default(int);
+                if (t2?.left != default(TreeNode))
+                    t1.left = MergeTrees(t1.left, t2.left);
+                if (t2?.right != default(TreeNode))
+                    t1.right = MergeTrees(t1.right, t2.right);
+            }
+            if (t1 == default(TreeNode))
+                return t2;
+            return t1;
+        }
+    }
+}
---
+++ ValidParentheses.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public static Hashtable table = new Hashtable();
+        static Solution()
+        {
+            table.Add('(', ')');
+            table.Add('{', '}');
+            table.Add('[', ']');
+        }
+
+        public bool IsValid(string s)
+        {
+            try
+            {
+                Stack<Char> stack = new Stack<Char>();
+                foreach (var bracket in s)
+                {
+                    if (table.ContainsKey(bracket))
+                        stack.Push(bracket);
+                    else
+                    {
+                        var tBracket = stack.Pop();
+                        if ((Char)table[tBracket] != bracket)
+                            throw new Exception();
+                    }
+                }
+                if (stack.Any())
+                    throw new Exception();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
